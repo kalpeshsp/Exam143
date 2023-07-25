bccbea44e5b9f6d09e396e919087c6b8c2e60238
Himanshu956 <kamanehimanshu76@gmail.com>
2023-07-25 08:11:02 +0530

    first commit for largestOfAll program

---
+++ FindLargest.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class FindLargest{
+
+    public static void main(String[] args) {
+           
+        Scanner sc=new Scanner(System.in);
+ 
+        System.out.println("Enter three numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+
+        int sum=a+b+c;
+        int max=a;
+
+        if(a>b&&a>c){
+            max=a;
+        }else if(b>c){
+            max=b;
+        }else{
+            max=c;
+        }
+
+        System.out.println("The maximum of three numbers is  "+max);
+
+        System.out.println("The sum of three numbers is  "+sum);
+
+    }
+}
